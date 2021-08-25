19c8aa258cef54d93f0e4ecb96b6329272c94647
Joranimus <primepostach@gmail.com>
2021-08-25 17:10:37 +0300

    commit

--- build/js/index.js
+++ build/js/index.js
@@ -1,5 +1,16 @@
 console.log('test');
 
 
+function showMenu() {
+   let target = document.getElementsByClassName('menu-wrap')[0];
+  
+   if (target.classList.contains('open')){
+    target.classList.remove('open');
+   } else {
+    target.classList.add('open');
+   }
+   
+   
+}
 
 
